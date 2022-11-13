b1da33fcc4506718d73aaea3c40eb7b1253edb98
vivekmodi1301 <modivivek03@gmail.com>
2022-11-13 11:10:25 +0530

    Firebase Login

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/messaging';
+import 'firebase/compat/firestore';
+
+// const app = firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// })
+// export const auth = app.auth()
+// export default app
+// import * as firebase from 'firebase';
+
+let config = {
+    apiKey: "AIzaSyB-Dyf7OyR--L4ZpoaI-kzB1D0AjFd9rJI",
+    authDomain: "auth-development-a9412.firebaseapp.com",
+    projectId: "auth-development-a9412",
+    storageBucket: "auth-development-a9412.appspot.com",
+    messagingSenderId: "95409650432",
+    appId : "1:95409650432:web:cad377841deaa6c84d11e5"
+};
+const app = firebase.initializeApp(config);
+export const auth = app.auth()
+export default firebase;
